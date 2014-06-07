06dc7179439ca15b4ca10e1f0d3baa61367713eb
xLaMbChOpSx <ryanskungfu@me.com>
2014-06-07 19:06:49 +1000

    Silent Sms Detection (Class 0), Database Restore, Progress Bar
    Sms Receiver will monitor for Class 0 messages and provide an alert when received,
    details are tracked to a database table and displayed in a notification fragment.
    
    Restore database tables using exported CSV files this will also be added into the
    Db Adapter shortly to allow data to persist across database updates. Database retore added to the RequestTask class so it can be executed in the background.
    
    Progress bar added to provide more feedback about background operations.

---
+++ app/src/main/java/com/SecUpwN/AIMSICD/fragments/SilentSmsFragment.java
@@ -0,0 +1,67 @@
+package com.SecUpwN.AIMSICD.fragments;
+
+import com.SecUpwN.AIMSICD.R;
+import com.SecUpwN.AIMSICD.adapters.AIMSICDDbAdapter;
+import com.SecUpwN.AIMSICD.adapters.BaseInflaterAdapter;
+import com.SecUpwN.AIMSICD.adapters.SilentSmsCardData;
+import com.SecUpwN.AIMSICD.adapters.SilentSmsCardInflater;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+public class SilentSmsFragment extends Fragment {
+
+    private AIMSICDDbAdapter mDbHelper;
+
+    public SilentSmsFragment() {}
+
+    @Override
+    public void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+        mDbHelper = new AIMSICDDbAdapter(getActivity().getBaseContext());
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.sms_fragment,
+                container, false);
+
+        if (view != null) {
+            ListView lv = (ListView) view.findViewById(R.id.list_view);
+/*            String originatingAddress = getArguments().getString("address");
+            String displayAddress = getArguments().getString("display_address");
+            String messageClass = getArguments().getString("message_class");
+            String serviceCentre = getArguments().getString("service_centre");
+            String messageBody = getArguments().getString("message");
+            int timestamp = getArguments().getInt("timestamp");*/
+            mDbHelper.open();
+            Cursor smsData = mDbHelper.getSilentSmsData();
+            BaseInflaterAdapter<SilentSmsCardData> adapter
+                    = new BaseInflaterAdapter<>(
+                    new SilentSmsCardInflater());
+            while (smsData.moveToNext()) {
+                SilentSmsCardData data = new SilentSmsCardData(smsData.getString(0), smsData.getString(1),
+                        smsData.getString(2), smsData.getString(3), smsData.getString(4),
+                        smsData.getInt(5));
+                adapter.addItem(data, false);
+            }
+            mDbHelper.close();
+            lv.setAdapter(adapter);
+            lv.setVisibility(View.VISIBLE);
+        }
+
+        return view;
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+    }
+
+}
