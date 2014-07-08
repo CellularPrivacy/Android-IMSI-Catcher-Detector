dc7c3376baaa0a371f78f9158c60052a6421f85b
xLaMbChOpSx <ryanskungfu@me.com>
2014-07-08 22:26:55 +1000

    Removed unused imports

--- app/src/main/java/com/SecUpwN/AIMSICD/utils/Device.java
+++ app/src/main/java/com/SecUpwN/AIMSICD/utils/Device.java
@@ -1,7 +1,6 @@
 package com.SecUpwN.AIMSICD.utils;
 
 import android.location.Location;
-import android.location.LocationManager;
 import android.os.Build;
 import android.telephony.CellIdentityCdma;
 import android.telephony.CellIdentityGsm;
