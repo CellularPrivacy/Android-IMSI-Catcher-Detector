44ab9725b3b731844b0489349166eb0acf2917c8
tobykurien <toby@tobykurien.com>
2014-12-07 10:34:04 +0200

    Added ability to  request a new OCID key from Preferences

--- app/src/main/java/com/SecUpwN/AIMSICD/activities/PrefActivity.java
+++ app/src/main/java/com/SecUpwN/AIMSICD/activities/PrefActivity.java
@@ -13,11 +13,20 @@ public class PrefActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        loadFragment();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        loadFragment();
+    }
+
+    private void loadFragment() {
         PrefFragment settingsFragment = new PrefFragment();
         FragmentManager fragmentManager = getFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(android.R.id.content, settingsFragment);
         fragmentTransaction.commit();
     }
-
 }
