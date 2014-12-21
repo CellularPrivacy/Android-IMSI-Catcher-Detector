0d4969e2b3092e3f1eabd1ed765abc9cf42c18c0
Tor Henning Ueland <tor.henning@gmail.com>
2014-12-21 18:36:05 +0100

    Created a BaseActivity class that handles icon update and moved from status-polling to Broadcasts to fetch status changes

--- app/src/main/java/com/SecUpwN/AIMSICD/utils/Status.java
+++ app/src/main/java/com/SecUpwN/AIMSICD/utils/Status.java
@@ -1,5 +1,9 @@
 package com.SecUpwN.AIMSICD.utils;
 
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
+
 /**
  * Class that sets, holds and returns current system status
  *
@@ -14,10 +18,21 @@ public class Status {
         IDLE,
     }
 
-    public static void setCurrentStatus(Type t) {
+    /*
+     * Changes the current status, this will also trigger a local broadcast event
+     * if the new status is different from the previous one
+     */
+    public static void setCurrentStatus(Type t, Context context) {
+        if(t != currentStatus) {
+            Intent intent = new Intent("StatusChange");
+            LocalBroadcastManager.getInstance(context).sendBroadcast(intent);
+        }
         currentStatus = t;
     }
 
+    /*
+     * Returns the current status
+     */
     public static Type getStatus() {
         if(currentStatus == null) {
             return Type.IDLE;
