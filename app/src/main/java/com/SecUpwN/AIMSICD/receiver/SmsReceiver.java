83dde483b522fce3c4341b263b60e447a2941f18
E:V:A <vonankh@gmail.com>
2015-02-10 06:58:30 +0200

    Trial fix of false 0SMS in SmsReceiver
    - attempted fix to false positived in SmsReceiver silent SMS detection.
    - some typos and several comments
    - removed the spooky Google permission: WRITE_GSERVICES
    (That one could make anyone paranoid!)

--- app/src/main/java/com/SecUpwN/AIMSICD/receiver/SmsReceiver.java
+++ app/src/main/java/com/SecUpwN/AIMSICD/receiver/SmsReceiver.java
@@ -13,6 +13,53 @@ import android.util.Log;
 import java.util.ArrayList;
 import java.util.List;
 
+
+/**
+ *  Description:    The SMS receiver class that handles the SMS PDU data
+ *
+ *  Dependencies:
+ *
+ *  Permissions:    To read and intercept various SMS/MMS/WAP push messages, we need
+ *                  at least 3 different Android permissions:
+ *
+ *                  android.permission.READ_SMS
+ *                  android.permission.RECEIVE_MMS
+ *                  android.permission.RECEIVE_SMS
+ *                  android.permission.RECEIVE_WAP_PUSH
+ *
+ *                  To read silent SMS from logcat, we also need:
+ *
+ *                  android.permission.READ_LOGS
+ *
+ *  Notes:      1)  3GPP TS 23.040 9.2.3.9 specifies that Type Zero messages are indicated
+ *                  by TP_PID field set to value 0x40 = 64 = 01000000
+ *              2)  For others see: http://web.tiscali.it/richard/tlc/articoli/sms3.htm
+ *              3) http://www.etsi.org/deliver/etsi_ts/123000_123099/123040/12.02.00_60/ts_123040v120200p.pdf
+ *              4)  Here:
+ *                          TP-MTI = Message Type Indicator    -
+ *                          TP-PID = Protocol IDentifier       -
+ *                          TP-MMS = More Messages to Send     -
+ *
+ *   Issues:
+ *              [ ] TODO: Add silent MMS check ? -- Is this correctly understood?
+ *              [ ] TODO: Add silent WAP PUSH check
+ *              [ ] TODO: https://github.com/SecUpwN/Android-IMSI-Catcher-Detector/issues/178
+ *              [ ]  Status Report Indication (TP-SRI) -- requested ?
+ *              [ ]  Validity period (TP-VP) -- too short ?
+ *              [ ]  The service centre time stamp (TP-SCTS) -- wrong date?
+ *
+ *      Log:
+ *      --------- beginning of /dev/log/main
+ *      I/AIMSICD_SmsReceiver(28544): Pdu data: firstByte = 8 mti = 0 TP_PID = 128
+ *      I/AIMSICD_SmsReceiver(28544): Type 0 Message received, Sender: +38631XXXXXX Message: t
+ *      I/CellTracker(28544): neighbouringCellInfo Size - 1
+ *      I/CellTracker(28544): neighbouringCellInfo - CID:-1 LAC:-1 RSSI:-85 PSC:183
+ *
+ *
+ *   ChangeLog:     2015-02-10  banjaxbanjo     - added code to dump full SMS PDU to logcat
+ *                  2015-02-11  E:V:A           - changed from "||" to "&& mti==0"
+ *
+ */
 public class SmsReceiver extends BroadcastReceiver {
 
     public void onReceive(Context context, Intent intent) {
@@ -25,48 +72,28 @@ public class SmsReceiver extends BroadcastReceiver {
                 String full_pdu_string = "";
                 for (Object pdu : pdus) {
                     byte smsPdu[] = (byte[]) pdu;
-                    /*
-                    Code for debugging and will dump the full sms in pdu format
-                    useful for logging to see the full pdu hexstring and easier 
-                    to parse / pinpoint elements TP-DCS TP-PID TP-MTI etc..
-                    */
-                    try{
-                        
-                            for(int xx = 0; xx < smsPdu.length;xx++)
-                            {
-                                String test = Integer.toHexString(smsPdu[xx] & 0xff);
-                                if (test.length() <= 1){test = "0"+test;}
-                                sb.append(test);
-                            }full_pdu_string = sb.toString();
-                        
-                        }catch (Exception err) {
-                            Log.e("SmsReceiver", "Exception Pdu smsReceiver" + err);
+
+                    // Dump the full SMS in PDU format (HEX string) to Logcat
+                    try {
+                        for(int xx = 0; xx < smsPdu.length; xx++) {
+                            String test = Integer.toHexString(smsPdu[xx] & 0xff);
+                            if (test.length() <= 1){ test = "0"+test; }
+                            sb.append(test);
                         }
-                    /**
-                     * 3GPP TS 23.040 9.2.3.9 specifies that Type Zero messages are indicated
-                     * by TP_PID field set to value 0x40
-                     */
+                        full_pdu_string = sb.toString();
+                    } catch (Exception err) {
+                        Log.e("SmsReceiver", "Exception PDU smsReceiver" + err);
+                    }
 
-                    /**
-                     * Bug: https://github.com/SecUpwN/Android-IMSI-Catcher-Detector/issues/178
-                     *
-                     * Log:
-                     * --------- beginning of /dev/log/mainI/AIMSICD_SmsReceiver(28544):
-                     * Pdu data: firstByte = 8 mti = 0 TP_PID = 128
-                     * I/AIMSICD_SmsReceiver(28544): Type 0 Message received, Sender: +38631XXXXXX
-                     * Message: t
-                     * I/CellTracker(28544): neighbouringCellInfo Size - 1
-                     * I/CellTracker(28544): neighbouringCellInfo - CID:-1 LAC:-1 RSSI:-85 PSC:183
-                     */
-
-                    // E:V:A  We may also need to consider catching WAP PUSH SMS messages
-                    // as they can also be hidden from user.
+                    // We may also need to consider catching WAP PUSH SMS messages
+                    // as they can also be hidden from user. -- E:V:A
                     int firstByte = smsPdu[0] & 0xff;
                     int mti = firstByte & 0x3;
-                    int pID = smsPdu[1] & 0xc0;
-                    Log.i("AIMSICD_SmsReceiver", "Pdu data: firstByte = " + firstByte +
+                    int pID = smsPdu[1] & 0xc0; // 192
+                    Log.i("AIMSICD_SmsReceiver", "PDU Data: firstByte = " + firstByte +
                             " mti = " + mti + " TP_PID = " + pID);
-                    if (pID == 0x40 || mti == 0) {
+                    // Need checking! --EVA
+                    if (pID == 0x40 && mti == 0) {
                         messages.add(SmsMessage.createFromPdu((byte[]) pdu));
                     }
                 }
@@ -75,15 +102,14 @@ public class SmsReceiver extends BroadcastReceiver {
                     for (SmsMessage sms : messages) {
                         Intent smsIntent = new Intent(CellTracker.SILENT_SMS);
                         Bundle smsData = new Bundle();
-                        smsData.putString("address", sms.getOriginatingAddress());
-                        smsData.putString("display_address",
-                                sms.getDisplayOriginatingAddress());
-                        smsData.putString("class", sms.getMessageClass().name());
-                        smsData.putString("service_centre", sms.getServiceCenterAddress());
-                        smsData.putString("message", sms.getMessageBody());
+                        smsData.putString("address",         sms.getOriginatingAddress());
+                        smsData.putString("display_address", sms.getDisplayOriginatingAddress());
+                        smsData.putString("class",           sms.getMessageClass().name());
+                        smsData.putString("service_centre",  sms.getServiceCenterAddress());
+                        smsData.putString("message",         sms.getMessageBody());
                         smsIntent.putExtras(smsData);
                         context.sendBroadcast(smsIntent);
-                        Log.i("AIMSICD_SmsReceiver", "Type 0 Message received, Sender: "
+                        Log.i("AIMSICD_SmsReceiver", "Type-0 SMS received! Sender: "
                                 + sms.getOriginatingAddress() + " Message: "
                                 + sms.getMessageBody());
                     }
