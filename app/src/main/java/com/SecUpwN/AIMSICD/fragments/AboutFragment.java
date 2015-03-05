b4a92467c045847eb9047bd41b0f9a5cefb35227
E:V:A <E3V3A@users.noreply.github.com>
2015-03-05 15:20:42 +0200

    Revert "CreditsRoll Initial Commit"

--- app/src/main/java/com/SecUpwN/AIMSICD/fragments/AboutFragment.java
+++ app/src/main/java/com/SecUpwN/AIMSICD/fragments/AboutFragment.java
@@ -3,8 +3,6 @@ package com.SecUpwN.AIMSICD.fragments;
 import com.SecUpwN.AIMSICD.R;
 
 import android.app.Activity;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
@@ -94,22 +92,9 @@ public class AboutFragment extends Fragment {
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                if (link !=  R.string.aimsicd_credits_link) {
-                    Intent intent;
-                    intent = new Intent(Intent.ACTION_VIEW,
-                            Uri.parse(getResources().getString(link)));
-                    startActivity(intent);
-                } else {
-                    try {
-                        getFragmentManager().beginTransaction()
-                                .replace(R.id.content_frame, new CreditFragment()).commit();
-                    } catch (Exception e) {
-                        //Log.i()
-                        e.toString();
-                        Exception foo = e;
-                    }
-                }
+                Intent intent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse(getResources().getString(link)));
+                startActivity(intent);
             }
         });
     }
@@ -122,4 +107,6 @@ public class AboutFragment extends Fragment {
 
     public AboutFragment() {
     }
+
+
 }
