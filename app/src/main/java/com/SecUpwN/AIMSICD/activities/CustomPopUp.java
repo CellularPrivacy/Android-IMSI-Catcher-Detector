32ac500693cbd8878200d67b3e59faa06b2e510b
Paul Kinsella <paulkinsella29@yahoo.ie>
2015-03-18 11:18:53 +0000

    Added new Custom Pop up Class

--- app/src/main/java/com/SecUpwN/AIMSICD/activities/CustomPopUp.java
+++ app/src/main/java/com/SecUpwN/AIMSICD/activities/CustomPopUp.java
@@ -89,25 +89,4 @@ public class CustomPopUp extends Activity {
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_about_pop_up, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
