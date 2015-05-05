78d6a076c11cc26a767275b8c792a95d7eada625
SecUpwN <SecUpwN@protonmail.ch>
2015-05-05 18:12:03 +0200

    Unified and resized copyright header

--- app/src/main/java/com/SecUpwN/AIMSICD/adapters/DbViewerSpinnerAdapter.java
+++ app/src/main/java/com/SecUpwN/AIMSICD/adapters/DbViewerSpinnerAdapter.java
@@ -1,18 +1,7 @@
-/* Android IMSI Catcher Detector
- *      Copyright (C) 2015
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You may obtain a copy of the License at
- *      https://github.com/SecUpwN/Android-IMSI-Catcher-Detector/blob/master/LICENSE
+/* Android IMSI-Catcher Detector | (c) AIMSICD Privacy Project
+ * -----------------------------------------------------------
+ * LICENSE:  http://git.io/vJaf6 | TERMS:  http://git.io/vJMf5
+ * -----------------------------------------------------------
  */
 package com.SecUpwN.AIMSICD.adapters;
 
