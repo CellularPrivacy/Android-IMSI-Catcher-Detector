9bbb1824579c2520eed3c326a77b2e829fe52976
Security: Pwned. <SecUpwN@users.noreply.github.com>
2015-05-08 12:03:06 +0000

    Fixed Toast Extender for #403

--- app/src/main/java/com/SecUpwN/AIMSICD/utils/Helpers.java
+++ app/src/main/java/com/SecUpwN/AIMSICD/utils/Helpers.java
@@ -143,8 +143,8 @@ import java.util.List;
 
       private Toaster() {}
 
-      public static void makeLongToast(String msg, long durationInMillis) {
-        final Toast t = Toast.makeText(App.context(), msg, Toast.LENGTH_SHORT);
+      public static void makeLongToast(Context context,String msg, long durationInMillis) {
+        final Toast t = Toast.makeText(context, msg, Toast.LENGTH_SHORT);
         t.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL, 0, 0);
 
         new CountDownTimer(Math.max(durationInMillis - SHORT_TOAST_DURATION, 1000), 1000) {
