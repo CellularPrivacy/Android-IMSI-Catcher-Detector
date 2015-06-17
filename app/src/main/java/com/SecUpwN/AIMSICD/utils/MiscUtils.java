e7169d3918869ffc73eaf061e61c3738d298aea2
Paul Kinsella <paulkinsella29@yahoo.ie>
2015-06-17 16:17:48 +0100

    Update MiscUtils.java

--- app/src/main/java/com/SecUpwN/AIMSICD/utils/MiscUtils.java
+++ app/src/main/java/com/SecUpwN/AIMSICD/utils/MiscUtils.java
@@ -11,12 +11,8 @@ import android.app.PendingIntent;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
-import android.widget.Toast;
-
 import com.SecUpwN.AIMSICD.AIMSICD;
 import com.SecUpwN.AIMSICD.R;
 import com.SecUpwN.AIMSICD.activities.CustomPopUp;
@@ -29,9 +25,8 @@ import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.net.URL;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
