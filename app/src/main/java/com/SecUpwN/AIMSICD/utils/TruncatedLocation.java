b8ce9fd874c8eedb729eb0887b08f0456c59b038
DJaeger <daniel-jaeger@online.de>
2015-07-02 16:44:38 +0200

    Repair OCIDCSV parsing

--- app/src/main/java/com/SecUpwN/AIMSICD/utils/TruncatedLocation.java
+++ app/src/main/java/com/SecUpwN/AIMSICD/utils/TruncatedLocation.java
@@ -1,6 +1,8 @@
 package com.SecUpwN.AIMSICD.utils;
 
 import android.location.Location;
+import java.text.NumberFormat;
+import java.text.ParseException;
 
 /**
  * Created by Marvin Arnold on 1/07/15.
@@ -26,7 +28,16 @@ public class TruncatedLocation extends Location {
     }
 
     public static double truncateDouble(double d, int numDecimal) {
+        double d = 0;
+        NumberFormat format = NumberFormat.getInstance();
+
         String s = String.format("%." + Integer.toString(numDecimal) +"f", d);
-        return Double.parseDouble(s);
+        try {
+            Number number = format.parse(s);
+            double d = number.doubleValue();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return d;
     }
 }
