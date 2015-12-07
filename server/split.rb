3e5c28a53ebb943a376df9bb2d06b8fe2d766f4e
Lars Grefer <github@larsgrefer.de>
2015-12-07 11:05:39 +0100

    Delete split.rb

--- server/split.rb
+++
@@ -1,9 +0,0 @@
-require 'csv'
-
-CSV.foreach('cell_towers.csv') do |row|
-  # radio,mcc,net,area,cell,unit,lon,lat,range,samples,changeable,created,updated,averageSignal
-  File.open("data/#{row[1]}.csv", 'a') do |json_writer|
-   json_writer.write "#{row[4]},#{row[6]},#{row[7]}\n"
-  end
-end
-
