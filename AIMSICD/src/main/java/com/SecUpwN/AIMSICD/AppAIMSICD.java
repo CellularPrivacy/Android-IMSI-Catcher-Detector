7c499d0b87a7811cb95db90cb1da650603620219
Security: Pwned <SecUpwN@users.noreply.github.com>
2015-12-29 00:41:58 +0000

    Added advanced bugsnag dependency
    Initialized Bugsnag

--- AIMSICD/src/main/java/com/SecUpwN/AIMSICD/AppAIMSICD.java
+++ AIMSICD/src/main/java/com/SecUpwN/AIMSICD/AppAIMSICD.java
@@ -23,6 +23,8 @@ import io.freefair.android.injection.modules.AndroidLoggerModule;
 import io.freefair.android.injection.modules.OkHttpModule;
 import io.freefair.android.util.logging.Logger;
 
+import com.bugsnag.android.*;
+
 public class AppAIMSICD extends InjectionApplication {
 
     @Inject
@@ -43,6 +45,8 @@ public class AppAIMSICD extends InjectionApplication {
         addModule(new AndroidLoggerModule());
         addModule(OkHttpModule.withCache(this));
         super.onCreate();
+        // Initializing Bugsnag to begin capturing exceptions
+        Bugsnag.init(this);
         TinyDB.getInstance().init(getApplicationContext());
         TinyDB.getInstance().putBoolean(TinyDbKeys.FINISHED_LOAD_IN_MAP, true);
     }
