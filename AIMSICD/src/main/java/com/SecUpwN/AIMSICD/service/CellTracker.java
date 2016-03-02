ac5240a4c826b1b7a27ea6219a231dc3f8a28a81
Vlad Todosin <vladtodosin@gmail.com>
2016-03-02 22:12:12 +0200

    Update CellTracker.java

--- AIMSICD/src/main/java/com/SecUpwN/AIMSICD/service/CellTracker.java
+++ AIMSICD/src/main/java/com/SecUpwN/AIMSICD/service/CellTracker.java
@@ -1000,15 +1000,13 @@ public class CellTracker implements SharedPreferences.OnSharedPreferenceChangeLi
             getApplication().setCurrentStatus(Status.OK, mVibrateEnabled, mVibrateMinThreatLevel);
             if (mTrackingFemtocell) {
                 contentText = context.getString(R.string.femtocell_detection_active);
-            } 
-            else
+            } else
             if (mTrackingCell){
                 contentText = context.getString(R.string.cell_tracking_active);
             } 
             if(mMonitoringCell){
                 contentText = context.getString(R.string.cell_monitoring_active);
-            } 
-            else{
+            } else {
                 getApplication().setCurrentStatus(Status.IDLE,mVibrateEnabled,mVibrateMinThreatLevel);
             }
         } else {
